d999170ec5ce578dbd100d2c66561ac65c9c5bc1
usuario1 <martinortiz2006@gmail.com>
2025-05-03 09:09:19 -0300

    agrego ALERT en el ejercicio7.js para cuando 2 0 los 3 numeros ingresados sean iguales

--- punto7/ejercicio7.js
+++ punto7/ejercicio7.js
@@ -8,23 +8,49 @@ let c = prompt("ingresa el tercer numero");
 
 if (a>b) {
     may = a;
-    if (a>c) {
-        may = a;
+    if (a<c) {
+        may=c;
     } else {
-        may = c;
+        if (a===c) {
+            alert ("el primer y el tercer numero son iguales");
+        } else {
+            if (c===b) {
+                alert("el segundo y el tercer numero son iguales")
+            }
+        }
     }
-} else{
-  if (a<b) {
-    may = b;
-    if (b<c) {
-        may = c;
+    alert("el mayor es: "+may);
+} else {
+    if (a===b) {
+        if (a===b===c) {
+            alert ("los tres numeros son iguales");
+        } else {
+            may=b;
+            if (b<c) {
+                alert ("el primero y el segundo son iguales");
+                may=c;
+            } else {
+                if (b===c) {
+                    alert ("el segundo y el tercer numero son iguales");
+                } else {
+                    alert ("el primero y el segundo son iguales");
+                }
+            }
+            alert ("el mayor es: "+may);
+        }
     } else {
-        may = b;
+        may=b;
+            if (b<c) {
+                may=c;
+            } else {
+                if (b===c) {
+                    alert ("el segundo y el tercer numero son iguales");
+                } else{
+                    if (c===a) {
+                        alert ("el primer y el tercer numero son iguales");
+                    }
+                }
+            }
+            alert ("el mayor es: "+may);
     }
-  } 
-
 }
-alert ("el mayor es :" + may);
-
-
-    
